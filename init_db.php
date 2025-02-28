44b6e3ca9a850812c1b86e45f74b34ecddc54887
Infyinfy <125547553+2002cactus@users.noreply.github.com>
2025-03-01 04:41:41 +0700

    Update init_db.php

--- init_db.php
+++ init_db.php
@@ -22,8 +22,8 @@ $query_create = "
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
