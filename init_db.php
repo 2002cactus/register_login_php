152b5ac8a9e3d36688ec0718bfdf8e38073e981c
Infyinfy <125547553+2002cactus@users.noreply.github.com>
2025-03-01 13:07:46 +0700

    Update init_db.php

--- init_db.php
+++ init_db.php
@@ -1,17 +1,6 @@
 <?php
-// Lấy thông tin database từ biến môi trường
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-
-// Kết nối đến PostgreSQL
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
-
-if (!$conn) {
-    die("❌ Lỗi kết nối database: " . pg_last_error());
-}
+// Lấy thông tin database
+require 'database.php';
 
 // Xóa bảng nếu đã tồn tại
 $query_drop = "DROP TABLE IF EXISTS users CASCADE;";
