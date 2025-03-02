3fbf6b3c08d7dfae1a4676ac241be1424e5e037a
Infyinfy <125547553+2002cactus@users.noreply.github.com>
2025-03-02 10:24:05 +0700

    Update save_users.php

--- save_users.php
+++ save_users.php
@@ -3,7 +3,7 @@
 require 'database.php';
 
 // Truy vấn lấy tất cả user
-$query = "SELECT id, name, username, email, password created_at FROM users";
+$query = "SELECT id, name, username, email, password FROM users";
 $result = pg_query($conn, $query);
 
 // Kiểm tra lỗi truy vấn
@@ -17,7 +17,7 @@ $data = "Danh sách User đã đăng ký:\n";
 
 // Ghi dữ liệu vào file
 while ($row = pg_fetch_assoc($result)) {
-    $data .= "ID: {$row['id']}, Name: {$row['name']}, Username: {$row['username']}, Email: {$row['email']}, Password: {$row['password']}, Created At: {$row['created_at']}\n";
+    $data .= "ID: {$row['id']}, Name: {$row['name']}, Username: {$row['username']}, Email: {$row['email']}, Password: {$row['password']}\n";
 }
 
 file_put_contents($file, $data);
